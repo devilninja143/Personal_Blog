35576e0ab0cc2ccd9abe89b206d8e0d3fbe6e906
devilninja143 <67749565+devilninja143@users.noreply.github.com>
2021-09-19 17:36:02 +0600

    Final

--- Personal_blog/settings.py
+++ Personal_blog/settings.py
@@ -137,6 +137,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,"/media")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 CORS_ALLOW_ALL_ORIGINS = True
