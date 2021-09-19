694e008039b87d78dd53e149fa25f0621c87a92c
devilninja143 <67749565+devilninja143@users.noreply.github.com>
2021-09-19 17:43:49 +0600

    Final

--- Personal_blog/urls.py
+++ Personal_blog/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("api/", include("blog.urls")),
     path('api-auth/', include('rest_framework.urls')),
     re_path(".*", TemplateView.as_view(template_name="index.html"))
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
