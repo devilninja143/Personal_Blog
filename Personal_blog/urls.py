7944f04de0fb4d05cbb72c1b3af89818b6f8a8d1
devilninja143 <67749565+devilninja143@users.noreply.github.com>
2021-09-19 17:58:11 +0600

    Final

--- Personal_blog/urls.py
+++ Personal_blog/urls.py
@@ -13,16 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from Personal_blog.Personal_blog.settings import MEDIA_ROOT
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("blog.urls")),
     path('api-auth/', include('rest_framework.urls')),
-    re_path(".*", TemplateView.as_view(template_name="index.html"))
+    re_path(".*", TemplateView.as_view(template_name="index.html")),
+    re_path(r"^media/(?P<path>.*)$", serve, {'document_root' :settings.MEDIA_ROOT})
 ]
 # +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
