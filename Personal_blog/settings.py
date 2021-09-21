dd6417398cdd6594cc0d904c12c3cc10151e880b
devilninja143 <67749565+devilninja143@users.noreply.github.com>
2021-09-21 20:50:17 +0600

    Last Test

--- Personal_blog/settings.py
+++ Personal_blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-jdqe26c-!+sbzy2foimaq=in@1q=7+h^vb@4%zsx+m!rwbhh#$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["creativeprogrammers.herokuapp.com"]
 
 
 # Application definition
